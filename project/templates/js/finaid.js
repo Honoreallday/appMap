b1df14f5190bc63db7bf7b1b7467ddb51fa77042
Emmanuel Honore <Emmanuelhk300@gmail.com>
2017-07-25 11:04:22 -0400

    First draft, Finaid cs and js

---
+++ project/templates/js/finaid.js
@@ -0,0 +1,16 @@
+
+/*function fades(){
+  $('#pt1').fadeTo(0,0);
+  $('#pt2').fadeTo(0,0);
+  $('#pt3').fadeTo(0,0);
+  $('.title').fadeTo(0,0);
+  $('#else').fadeTo(0,0);
+  $('.title').delay(400).fadeTo(2500,1);
+  $('#pt1').delay(1550).fadeTo(2500,1);
+  $('#pt2').delay(2700).fadeTo(2500,1);
+  $('#pt3').delay(3850).fadeTo(2500,1);
+  $('#else').delay(5000).fadeTo(0,1);
+  console.log('it woks');
+};
+
+$(document).ready(fades);
