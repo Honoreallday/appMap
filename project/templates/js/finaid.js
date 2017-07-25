b0d31c20bb8b960e9eb24b3dc5501b253fc83d4f
Emmanuel Honore <Emmanuelhk300@gmail.com>
2017-07-25 11:08:17 -0400

    Minor change, accidently left the js commented out

--- project/templates/js/finaid.js
+++ project/templates/js/finaid.js
@@ -1,5 +1,6 @@
-
-/*function fades(){
+/* the fade in function for the title, it takes a bit to load the whole page because of it so just comment out when your working
+on css and html */
+function fades(){
   $('#pt1').fadeTo(0,0);
   $('#pt2').fadeTo(0,0);
   $('#pt3').fadeTo(0,0);
